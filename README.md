3d269eefbf05cedf696547cf9f17dc7e5238facd
Adi Narayana Reddy <adinarayana4444@gmail.com>
2023-04-08 21:56:48 +0530

    Completed

---
+++ README.md
@@ -0,0 +1,51 @@
+# Spotify Clone
+
+Spotify Clone using Vite React.
+
+## Tech Stack
+
+**Tech:** Vite React
+
+**Api's** : Graphql
+
+**Packages:** react-loading-skeleton, @apollo/client
+
+## To Run This Project Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/adi-4444/spotify-clone
+```
+
+Go to the project directory
+
+```bash
+  cd spotify-clone
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run dev
+```
+
+## Lessons Learned
+
+One possible implementation to enhance the user experience of a music player website/application is to dynamically change the body background based on the selected song. In this approach, the most dominant color from the song's cover photo is extracted and applied as a linear-gradient to the body background.
+
+When ever the tab or navigation changes then song should remains playing.
+
+## 🚀 About Me
+
+I'm a Frontend Developer
+
+## Contact
+
+To Connect, email @ adinarayana4444@gmail.com
--- src/components/Player/Seeker.jsx
+++ src/components/Player/Seeker.jsx
@@ -70,6 +70,34 @@ export default function Seeker({ audio, isPlaying }) {
 		function handleMouseUp() {
 			isDraggingRef.current = false;
 		}
+
+		function handleTouchStart(event) {
+			event.preventDefault();
+			isDraggingRef.current = true;
+			const canvas = canvasRef.current;
+			const rect = canvas.getBoundingClientRect();
+			const x = event.touches[0].clientX - rect.left;
+			const progress = x / canvas.width;
+			if (audio) {
+				audio.currentTime = progress * audio.duration;
+			}
+		}
+
+		function handleTouchMove(event) {
+			event.preventDefault();
+			if (isDraggingRef.current) {
+				const { pageX } = event.touches[0];
+				const rect = canvasRef.current.getBoundingClientRect();
+				const offsetX = pageX - rect.left;
+				const progress = offsetX / width;
+				audio.currentTime = audio.duration * progress;
+				draw();
+			}
+		}
+
+		function handleTouchEnd() {
+			isDraggingRef.current = false;
+		}
 		if (isPlaying) {
 			animate();
 		} else {
@@ -80,12 +108,18 @@ export default function Seeker({ audio, isPlaying }) {
 		canvas.addEventListener("mousedown", handleMouseDown);
 		canvas.addEventListener("mousemove", handleMouseMove);
 		canvas.addEventListener("mouseup", handleMouseUp);
+		canvas.addEventListener("touchstart", handleTouchStart);
+		canvas.addEventListener("touchmove", handleTouchMove);
+		canvas.addEventListener("touchend", handleTouchEnd);
 
 		return () => {
 			cancelAnimationFrame(animationFrameId);
 			canvas.removeEventListener("mousedown", handleMouseDown);
 			canvas.removeEventListener("mousemove", handleMouseMove);
 			canvas.removeEventListener("mouseup", handleMouseUp);
+			canvas.addEventListener("touchstart", handleTouchStart);
+			canvas.addEventListener("touchmove", handleTouchMove);
+			canvas.addEventListener("touchend", handleTouchEnd);
 		};
 	}, [audio, isPlaying]);
 
